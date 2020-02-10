8ce6fdf92f3b854ed2301e16c208570855b96ce1
Ponomarev Sergei <papasergiusv@gmail.com>
2020-02-10 22:15:16 +0300

    Fix navbar. Change hours input to range input

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const getRangeValue = () => {
+  let hours = document.getElementById("hours-range").value;
+  document.getElementById("hours-input").value = hours;
+  document.getElementById("hours-label").innerHTML = hours;
+};
